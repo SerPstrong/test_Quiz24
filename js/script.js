737ff7c9d7944a3b8086f57961c87415fa8d5b22
Sergey Plotnikov <ommuts@gmail.com>
2021-03-08 22:16:58 +0500

    full

--- js/script.js
+++ js/script.js
@@ -1,7 +1,31 @@
+$(function () {
+    $("#tel").mask("+7 (999) 999-99-99");
+});
 
-window.randomize = function () {
-    $('.ko-progress-circle').attr('data-progress', 100);
+setTimeout(function () {
+
+    var time = 100;
+    var initialOffset = '440';
+    var i = 1
+
+    $('.circle_animation').css('stroke-dashoffset', initialOffset - (1 * (initialOffset / time)));
+
+    var interval = setInterval(function () {
+        $('.vl').text(i+"%");
+        if (i === time) {
+            clearInterval(interval);
+            return;
+        }
+        $('.circle_animation').css('stroke-dashoffset', initialOffset - ((i + 1) * (initialOffset / time)));
+        i++;
+    }, 30);
+
+}, 0)
+
+let value = document.querySelector(".vl");
+
+function val() {
+    return value.innerHTML = "<img src=\"img/tick.png\" alt=\"tick\">";
 }
-setTimeout(window.randomize, 100);
-$('.ko-progress-circle').click(window.randomize);
 
+setTimeout(val, 3500);
